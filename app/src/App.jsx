203ad607ef5de830c3d1272435f98824ef0f53f5
Aishwary P <aishwaryp@Aishwarys-MacBook-Air.local>
2024-03-05 14:40:22 +0530

    portal added

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createPortal } from 'react-dom'
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const decrement = () => {
+    setCount(count - 1);
+    toast('Opps ! Count Decremented!');
+  }
+  const increment = () => {
+    setCount(count - 1);
+    toast('🦄 Wow Count Incremented!');
+  }
   return (
     <>
       <div>
@@ -17,15 +25,34 @@ function App() {
         </a> */}
       </div>
       <h1>React Create Portal</h1>
+      <p>The below toaster is kept outside the virtual dom using create Portal we can directly access the Real DOM</p>
       <div className="card" style={{display:"flex" , justifyContent:"center" , gap:"25px"}}>
-        <button style={{backgroundColor:"red"}} onClick={() => setCount((count) => count - 1)}>
+        <button style={{backgroundColor:"red"}} onClick={decrement}>
           -
         </button>
         <p>{count}</p>
-        <button style={{backgroundColor:"green"}} onClick={() => setCount((count) => count + 1)}>
+        <button style={{backgroundColor:"green"}} onClick={increment}>
           +
         </button>
-      </div>
+      </div> 
+      {
+        createPortal(
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            limit={4}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          /> ,
+          document.getElementById('portal')
+        )
+      }
     </>
   )
 }
